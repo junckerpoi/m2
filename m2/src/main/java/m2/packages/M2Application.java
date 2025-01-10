b4b565e302c0e3fdbcf6dcb2fdf73a2bd5cde51d
panda <duremarkastu@gmail.com>
2025-01-10 23:20:28 +0300

    package re-routing

--- m2/src/main/java/m2/packages/M2Application.java
+++ m2/src/main/java/m2/packages/M2Application.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package m2.packages;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class M2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(M2Application.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(M2Application.class, args);
+  }
 }
--- m2/src/test/java/m2/packages/M2ApplicationTests.java
+++ m2/src/test/java/m2/packages/M2ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package m2.packages;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
