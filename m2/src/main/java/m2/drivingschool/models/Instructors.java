c22feb51ea87c26f1dc96ed153b9d5b72db4645f
Duressa Markos <duremarkastu@gmail.com>
2025-02-02 22:33:32 +0300

    added Instructor table

--- m2/src/main/java/m2/drivingschool/models/Instructors.java
+++ m2/src/main/java/m2/drivingschool/models/Instructors.java
@@ -1,5 +1,114 @@
 package m2.drivingschool.models;
 
+import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+
+@Entity
 public class Instructors {
-    
+    @Id
+    private String instructorId;
+
+    private String firstName;
+    private String lastName;
+
+    private String phoneNumber;
+    private String email;
+
+    private Date dateOfBirth;
+    private Date hireDate;
+
+    private String licenseNumber; // Teaching license
+
+    @Enumerated(EnumType.STRING)
+    private InstructorType instructorType;
+
+    private Boolean status;
+
+    public enum InstructorType {
+        THEORY,
+        PRACTICAL,
+    }
+
+    public String getInstructorId() {
+        return instructorId;
+    }
+
+    public void setInstructorId(String instructorId) {
+        this.instructorId = instructorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public InstructorType getInstructorType() {
+        return instructorType;
+    }
+
+    public void setInstructorType(InstructorType instructorType) {
+        this.instructorType = instructorType;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
 }
