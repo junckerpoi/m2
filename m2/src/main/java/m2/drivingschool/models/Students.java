80015a8082fbd7441ae13411b67c021f3ee42690
Duressa Markos <duremarkastu@gmail.com>
2025-02-02 11:58:21 +0300

    students model

--- m2/src/main/java/m2/drivingschool/models/Students.java
+++ m2/src/main/java/m2/drivingschool/models/Students.java
@@ -1,5 +1,105 @@
 package m2.drivingschool.models;
 
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Students {
-    
+    @Id
+    private String studentId;
+
+    private String firstName;
+    private String lastName;
+
+    private String phoneNumber;
+    private String email;
+
+    private Date dateOfBirth;
+    private String address;
+    private Date regestrationDate;
+    private String licenseNumber;
+    private Boolean status;
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getRegestrationDate() {
+        return regestrationDate;
+    }
+
+    public void setRegestrationDate(Date regestrationDate) {
+        this.regestrationDate = regestrationDate;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
 }
