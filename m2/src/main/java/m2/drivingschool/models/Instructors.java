641c373327fb4856f7d563f9a051d2cb57dab6ae
Duressa Markos <duremarkastu@gmail.com>
2025-02-03 12:47:19 +0300

    employee and instructors one to one relation if employee role = instructor

--- m2/src/main/java/m2/drivingschool/models/Instructors.java
+++ m2/src/main/java/m2/drivingschool/models/Instructors.java
@@ -1,31 +1,28 @@
 package m2.drivingschool.models;
 
-import java.util.Date;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class Instructors {
     @Id
-    private String instructorId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer instructorId;
 
-    private String firstName;
-    private String lastName;
-
-    private String phoneNumber;
-    private String email;
-
-    private Date dateOfBirth;
-    private Date hireDate;
+    @OneToOne
+    @JoinColumn(name = "employeeId", referencedColumnName = "employeeId")
+    private Employees employees;
 
     private String licenseNumber; // Teaching license
 
     @Enumerated(EnumType.STRING)
     private InstructorType instructorType;
-
-    private Boolean status;
 
     public enum InstructorType {
         THEORY,
@@ -38,54 +35,6 @@ public class Instructors {
 
     public void setInstructorId(String instructorId) {
         this.instructorId = instructorId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Date getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(Date dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public Date getHireDate() {
-        return hireDate;
-    }
-
-    public void setHireDate(Date hireDate) {
-        this.hireDate = hireDate;
     }
 
     public String getLicenseNumber() {
@@ -104,11 +53,4 @@ public class Instructors {
         this.instructorType = instructorType;
     }
 
-    public Boolean getStatus() {
-        return status;
-    }
-
-    public void setStatus(Boolean status) {
-        this.status = status;
-    }
 }
